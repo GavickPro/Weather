1080b45a8a267568b8495c0aa61a55b5fd4989cc
bkrztuk <bartlomiej.krztuk@gmail.com>
2012-12-17 16:48:38 +0100

    J!3.0 compatible initial commit - finished backend

--- mod_weather_gk4/admin/elements/about.php
+++ mod_weather_gk4/admin/elements/about.php
@@ -8,6 +8,6 @@ class JFormFieldAbout extends JFormField {
 	protected $type = 'About';
 
 	protected function getInput() {
-		return '<div id="gk_about_us">' . JText::_('MOD_TABS_ABOUT_US_CONTENT') . '</div>';
+		return '<div id="gk_about_us">' . JText::_('MOD_TABS_ABOUT_US_CONTENT') . '</div></div>';
 	}
 }
--- mod_weather_gk4/admin/script.js
+++ mod_weather_gk4/admin/script.js
@@ -1,52 +1,43 @@
-window.addEvent("domready",function(){
+jQuery(document).ready(function() {
 	getUpdates();
+	
 	// other form operations
-	$$('.input-pixels').each(function(el){el.getParent().innerHTML = el.getParent().innerHTML + "<span class=\"unit\">px</span>"});
-	$$('.input-percents').each(function(el){el.getParent().innerHTML = el.getParent().innerHTML + "<span class=\"unit\">%</span>"});
-	$$('.input-minutes').each(function(el){el.getParent().innerHTML = el.getParent().innerHTML + "<span class=\"unit\">minutes</span>"});
-	$$('.input-ms').each(function(el){el.getParent().innerHTML = el.getParent().innerHTML + "<span class=\"unit\">ms</span>"});
-	// switchers
-	$$('.gk_switch').each(function(el){
-		el.setStyle('display','none');
-		var style = (el.value == 1) ? 'on' : 'off';
-		var switcher = new Element('div',{'class' : 'switcher-'+style});
-		switcher.inject(el, 'after');
-		switcher.addEvent("click", function(){
-			if(el.value == 1){
-				switcher.setProperty('class','switcher-off');
-				el.value = 0;
-			}else{
-				switcher.setProperty('class','switcher-on');
-				el.value = 1;
-			}
-		});
-	});
+	$$('.input-minutes').each(function(el){el.getParent().innerHTML = "<div class=\"input-prepend\">" + el.getParent().innerHTML + "<span class=\"add-on\">minutes</span></div>"});
+	document.id('gk_module_updates').getParent().setStyle('margin-left', '20px');	
+	document.id('gk_about_us').getParent().setStyle('margin-left', '20px');
 	
-	
-	
-	// demo link
-	new Element('a', { 'href' : 'http://sigizmund.info/woeidinfo/', 'target' : '_blank',  'id' : 'gkDemoLink', 'html' : 'WOEID'  }).inject(document.id('jform_params_WOEID'), 'after');
+	// WOEID link
+	var woeidLink = jQuery('<a>', { 'href' : 'http://sigizmund.info/woeidinfo/', 'target' : '_blank',  'id' : 'gkDemoLink', 'html' : 'WOEID'});
+	jQuery('#jform_params_WOEID').after(woeidLink);
 	// help link
-	var link = new Element('a', { 'class' : 'gkHelpLink', 'href' : 'http://tools.gavick.com/weather.html', 'target' : '_blank' })
-	link.inject($$('div.panel')[$$('div.panel').length-1].getElement('h3'), 'bottom');
-	link.addEvent('click', function(e) { e.stopPropagation(); });
-});
-// function to generate the updates list
-function getUpdates() {
-	document.id('jform_params_module_updates-lbl').destroy(); // remove unnecesary label
-	var update_url = 'https://www.gavick.com/updates.raw?task=json&tmpl=component&query=product&product=mod_gk_weather_j16';
-	var update_div =document.id('gk_module_updates');
-	update_div.innerHTML = '<div id="gk_update_div"><span id="gk_loader"></span>Loading update data from GavicPro Update service...</div>';
-	
-	new Asset.javascript(update_url,{
-		id: "new_script",
-		onload: function(){
-			content = '';
-			$GK_UPDATE.each(function(el){
-				content += '<li><span class="gk_update_version"><strong>Version:</strong> ' + el.version + ' </span><span class="gk_update_data"><strong>Date:</strong> ' + el.date + ' </span><span class="gk_update_link"><a href="' + el.link + '" target="_blank">Download</a></span></li>';
-			});
-			update_div.innerHTML = '<ul class="gk_updates">' + content + '</ul>';
-			if(update_div.innerHTML == '<ul class="gk_updates"></ul>') update_div.innerHTML = '<p>There is no available updates for this module</p>';	
-		}
+	var link = jQuery('<small>', { 'class' : 'gkHelpLink', 'href' : 'https://www.gavick.com/weather-gk4.html', 'target' : '_blank' });
+	jQuery(jQuery('ul.nav-tabs li')[6]).find('a').append(link);
+	link.click( function(e) { 
+		e.preventDefault(); 
+		e.stopPropagation(); 
+		document.location.href = link.attr('href');
 	});
+});
+
+
+
+function getUpdates() {
+	jQuery('#jform_params_template_updates-lbl').remove(); // remove unnecesary label
+	var update_url = 'https://www.gavick.com/updates.raw?task=json&tmpl=component&query=product&product=mod_weather_gk4_j30';
+	var update_div = jQuery('#gk_module_updates');
+	update_div.html('<div id="gk_update_div"><span id="gk_loader"></span>Loading update data from GavicPro Update service...</div>');
+
+	jQuery.getScript(update_url, function(data, textStatus, jqxhr) {
+		jQuery($GK_UPDATE).each(function(i, el){								        
+            content += '<li><span class="gk_update_version"><strong>Version:</strong> ' + el.version + ' </span><span class="gk_update_data"><strong>Date:</strong> ' + el.date + ' </span><span class="gk_update_link"><a href="' + el.link + '" target="_blank">Download</a></span></li>';
+	     });
+	         
+        update_div.html('<ul class="gk_updates">' + content + '</ul>');
+        if(update_div.html() == '<ul class="gk_updates">[object Window]</ul>') {
+        	update_div.html('<p>There is no available updates for this module</p>'); 
+    	}
+    	if(update_div.html() == '<ul class="gk_updates"></ul>') {
+    		update_div.html('<p>There is no available updates for this module</p>'); 
+    	}
+	  });	
 }
